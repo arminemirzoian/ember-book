5fe18efb08a7da078ae67fb791812113b7264691
Armine <amirzoyan@griddynamics.com>
2022-09-28 19:13:55 +0400

    advanced routing added

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -7,6 +7,29 @@ export default class BandsNewController extends Controller {
   @service catalog;
   @service router;
   @tracked name;
+
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', (transition) => {
+      if (transition.isAborted) {
+        return;
+      }
+      if (this.confirmedLeave) {
+        return;
+      }
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   updateName(event) {
     this.name = event.target.value;
@@ -14,6 +37,7 @@ export default class BandsNewController extends Controller {
   @action
   async saveBand() {
     let band = await this.catalog.create('band', { name: this.name });
+    this.confirmedLeave = true;
     this.router.transitionTo('bands.band.songs', band.id);
   }
 }
--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default class BandsBandRoute extends Route {
   @service catalog;
+
   model(params) {
     return this.catalog.find('band', (band) => band.id === params.id);
   }
